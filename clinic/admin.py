6fd145f01b666fbf60e3d2776190a0b7e232e924
baganthar <thetkhaingaung1996@gmail.com>
2021-12-04 09:38:43 +0630

    1 commit

---
+++ clinic/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+admin.site.site_header = 'BAGAN'
+# Register your models here.
+from .models import Clinic
+
+
+class ClinicAdmin(admin.ModelAdmin):
+	list_display = ['patient_name', 'patient_phone', 'patient_address', 'patient_case', 'drug', 'fees', 'doctor', 'datetime']
+	list_filter = ['patient_address', 'patient_case', 'doctor', 'drug', 'fees', 'datetime']
+	search_fields = ['patient_name']
+
+admin.site.register(Clinic, ClinicAdmin)
